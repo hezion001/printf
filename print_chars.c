014cba02a03bee0fe078f589ea6d2670eebdf871
hezion001 <ibok.hezekiah@gmail.com>
2022-10-18 11:35:14 +0100

    print_r function created

--- print_chars.c
+++ print_chars.c
@@ -60,7 +60,6 @@ static int hex_print(char c)
 /**
  * print_S - prints a string and nonprintable character ascii values
  * @S: string to print
- *
  * Return: number of chars printed
  */
 int print_S(va_list S)
@@ -89,3 +88,26 @@ int print_S(va_list S)
 	return (count);
 }
 
+/**
+ * print_r - prints astring in reverse
+ * @r: string to print
+ *
+ * Return: number of chars printed
+ */
+int print_r(va_list r)
+{
+	char *str;
+	int i, count = 0;
+
+	str = va_arg(r, char *);
+	if (str == NULL)
+		str = ")llun(";
+	for (i = 0; str[i]; i++)
+		;
+	for (i -= 1; i >= 0; i--)
+	{
+		_putchar(str[i]);
+		count++;
+	}
+	return (count);
+}
