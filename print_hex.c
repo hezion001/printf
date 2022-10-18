852799584a3a34446e5b7cc7f1d773c3a36e93b5
hezion001 <ibok.hezekiah@gmail.com>
2022-10-18 11:50:04 +0100

    print_p function updated

--- print_hex.c
+++ print_hex.c
@@ -1,10 +1,9 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * print_hex - prints an unsigned int in hexidecimal form
  * @n: unsigned int to print
  * @c: flag to determine case of printing (0 = lower, 1 = upper)
- *
  * Return: number of digits printed
  */
 int print_hex(unsigned int n, unsigned int c)
@@ -53,7 +52,6 @@ int print_x(va_list x)
 /**
  * print_X - takes am unsigned int and prints it in uppercase hex notation
  * @X: unsigned int to print
- *
  * Return: number of digits printed
  */
 int print_X(va_list X)
@@ -65,7 +63,6 @@ int print_X(va_list X)
  * _pow - calculates an exponent
  * @base: base of exponent
  * @exponent: exponent of number
- *
  * Return: base ^ exponent
  */
 static unsigned long _pow(unsigned int base, unsigned int exponent)
@@ -78,4 +75,52 @@ static unsigned long _pow(unsigned int base, unsigned int exponent)
 		ans *= base;
 	}
 	return (ans);
+}
+
+/**
+ * print_p - prints an address
+ * @p: address to print
+ * Return: number of characters to print
+ */
+int print_p(va_list p)
+{
+	int count = 0;
+	unsigned int a[16];
+	unsigned int i, sum;
+	unsigned long n, m;
+	char *str = "(nil)";
+
+	n = va_arg(p, unsigned long);
+	if (n == 0)
+	{
+		for (i = 0; str[i]; i++)
+		{
+			_putchar(str[i]);
+			count++;
+		}
+		return (count);
+	}
+	_putchar('0');
+	_putchar('x');
+	count = 2;
+	m = _pow(16, 15); /* 16 ^ 15 */
+	a[0] = n / m;
+	for (i = 1; i < 16; i++)
+	{
+		m /= 16;
+		a[i] = (n / m) % 16;
+	}
+	for (i = 0, sum = 0; i < 16; i++)
+	{
+		sum += a[i];
+		if (sum || i == 15)
+		{
+			if (a[i] < 10)
+				_putchar('0' + a[i]);
+			else
+				_putchar('0' + ('a' - ':') + a[i]);
+			count++;
+		}
+	}
+	return (count);
 }
