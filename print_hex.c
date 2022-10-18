8f0b1f16b21f13f2c065dcaf600f68e016674688
hezion001 <ibok.hezekiah@gmail.com>
2022-10-18 11:42:20 +0100

    print_x function written

--- print_hex.c
+++ print_hex.c
@@ -39,3 +39,14 @@ int print_hex(unsigned int n, unsigned int c)
 	}
 	return (count);
 }
+
+/**
+ * print_x - takes an unsigned int and prints it in lowercase hex notation
+ * @x: unsigned int to print
+ * Return: number of digits printed
+ */
+int print_x(va_list x)
+{
+	return (print_hex(va_arg(x, unsigned int), 0));
+}
+
