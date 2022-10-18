b18fbd42eee3642238e3f4b4816f317c6d13037a
hezion001 <ibok.hezekiah@gmail.com>
2022-10-18 11:46:43 +0100

    print_X program written

--- print_hex.c
+++ print_hex.c
@@ -50,3 +50,14 @@ int print_x(va_list x)
 	return (print_hex(va_arg(x, unsigned int), 0));
 }
 
+/**
+ * print_X - takes am unsigned int and prints it in uppercase hex notation
+ * @X: unsigned int to print
+ *
+ * Return: number of digits printed
+ */
+int print_X(va_list X)
+{
+	return (print_hex(va_arg(X, unsigned int), 1));
+}
+
